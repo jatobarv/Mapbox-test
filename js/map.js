b542214b7382e6d59f1a80da79b92ad8a54d6508
Javier Tobar <ja.tobarv@alumnos.duoc.cl>
2019-05-16 23:24:44 -0400

    Agrega marcadores al mapa

--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
+
 mapboxgl.accessToken =
     "pk.eyJ1IjoiamF2aWVydG9iYXIiLCJhIjoiY2p1dDlndG80MDVyZzQ0bWh1aXduczlyNiJ9.ECrS8-CoO1Ura4mmYo865A";
 var map = new mapboxgl.Map({
@@ -27,3 +28,48 @@ map.addControl(
     }),
     "top-left"
 );
+
+var geojson = {
+    type: 'FeatureCollection',
+    features: [{
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-70.7631, -33.5209]
+        },
+        properties: {
+            title: 'Mapbox',
+            description: 'Washington, D.C.'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [-70.7346887, -33.4971777]
+        },
+        properties: {
+            title: 'Mapbox',
+            description: 'San Francisco, California'
+        }
+    }]
+};
+
+// add markers to map
+geojson.features.forEach(function (marker) {
+
+    // create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map);
+
+    new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+        .addTo(map);
+});
